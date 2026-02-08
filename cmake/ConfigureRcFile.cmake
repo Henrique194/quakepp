a5949171d4ee3dbe9d6bf5f83158dfee6d55581f
Henrique Jorge Barateli <henriquejb194@gmail.com>
2026-02-08 10:24:31 -0300

    build: simplify CMake files

--- cmake/ConfigureRcFile.cmake
+++ cmake/ConfigureRcFile.cmake
@@ -1,13 +1,12 @@
-function(configure_rc_file)
+function(configure_rc_file rc_name rc_dir)
   string(CONCAT RC_VERSION
       "${PROJECT_VERSION_MAJOR}, "
       "${PROJECT_VERSION_MINOR}, "
       "${PROJECT_VERSION_PATCH}, "
       "0"
   )
-  set(RC_NAME "${PACKAGE_TARNAME}.rc")
-  set(RC_INPUT_FILE "${WIN_DIR}/${RC_NAME}.in")
-  set(RC_OUTPUT_FILE "${CMAKE_CURRENT_BINARY_DIR}/${RC_NAME}")
+  set(RC_INPUT_FILE "${rc_dir}/${rc_name}.in")
+  set(RC_OUTPUT_FILE "${CMAKE_CURRENT_BINARY_DIR}/${rc_name}")
 
   configure_file(${RC_INPUT_FILE} ${RC_OUTPUT_FILE})
   target_sources(${PACKAGE_TARNAME} PUBLIC ${RC_OUTPUT_FILE})
