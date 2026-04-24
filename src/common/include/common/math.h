f05911ace7552ca2c8d1d54e362df07efe2bb3ef
Henrique Jorge Barateli <henriquejb194@gmail.com>
2026-04-24 15:18:15 -0300

    refactor: improve vector implementation

--- src/common/include/common/math.h
+++ src/common/include/common/math.h
@@ -20,42 +20,98 @@
 #pragma once
 
 #include "common/assert.h"
+#include <array>
 #include <cmath>
 
-struct Vec3 {
-    float x{0};
-    float y{0};
-    float z{0};
+template<size_t N>
+requires(N > 0)
+class Vec {
+  public:
+    Vec() = default;
+
+    template<typename... Args, typename = std::enable_if_t<sizeof...(Args) == N>>
+    Vec(Args... args)
+        : data{static_cast<float>(args)...} {
+    }
+
+    float& x() {
+        return data[0];
+    }
+
+    [[nodiscard]]
+    float x() const {
+        return data[0];
+    }
+
+    template<size_t M = N>
+    requires(M > 1)
+    float& y() {
+        return data[1];
+    }
+
+    template<size_t M = N>
+    requires(M > 1)
+    [[nodiscard]]
+    float y() const {
+        return data[1];
+    }
+
+    template<size_t M = N>
+    requires(M > 2)
+    float& z() {
+        return data[2];
+    }
+
+    template<size_t M = N>
+    requires(M > 2)
+    [[nodiscard]]
+    float z() const {
+        return data[2];
+    }
+
+    [[nodiscard]]
+    float dot(const Vec& v) const {
+        float result{0};
+        for (size_t i = 0; i < N; ++i) {
+            result += data[i] * v[i];
+        }
+        return result;
+    }
+
+    template<size_t M = N>
+    requires(M == 2)
+    float perp_dot(const Vec& v) const {
+        return (x() * v.y()) - (y() * v.x());
+    }
+
+    template<size_t M = N>
+    requires(M == 3)
+    Vec cross(const Vec& v) const {
+        float x_val{y() * v.z() - z() * v.y()};
+        float y_val{z() * v.x() - x() * v.z()};
+        float z_val{x() * v.y() - y() * v.x()};
+        return Vec{x_val, y_val, z_val};
+    }
 
     [[nodiscard]]
     float length() const {
-        return sqrtf(length_squared());
+        return std::sqrtf(length_squared());
     }
 
     [[nodiscard]]
     float length_squared() const {
-        return this->dot(*this);
+        return dot(*this);
     }
 
     [[nodiscard]]
-    float dot(const Vec3& v) const {
-        return (x * v.x) + (y * v.y) + (z * v.z);
-    }
-
-    [[nodiscard]]
-    Vec3 cross(const Vec3& v) const {
-        return Vec3{y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x};
-    }
-
-    [[nodiscard]]
-    Vec3 normalize() const {
-        Vec3 normalized{(1.0f / length()) * *this};
+    Vec normalize() const {
+        Vec normalized{(1.0f / length()) * *this};
         Q_DASSERT(normalized.is_finite());
         return normalized;
     }
 
     [[nodiscard]]
-    Vec3 fast_renormalize() const {
+    Vec fast_renormalize() const {
         // Numerically approximate the inverse square
         // root by a Taylor series around 1.
         return *this * (0.5f * (3.0f - length_squared()));
@@ -63,39 +119,71 @@ struct Vec3 {
 
     [[nodiscard]]
     bool is_finite() const {
-        return std::isfinite(x) && std::isfinite(y) && std::isfinite(z);
+        for (size_t i = 0; i < N; ++i) {
+            if (!std::isfinite(data[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    Vec3 operator+(const Vec3& v) const {
-        return Vec3{x + v.x, y + v.y, z + v.z};
+    float& operator[](size_t i) {
+        Q_DASSERT(i < N);
+        return data[i];
     }
 
-    void operator+=(const Vec3& v) {
+    float operator[](size_t i) const {
+        Q_DASSERT(i < N);
+        return data[i];
+    }
+
+    Vec operator+(const Vec& v) const {
+        Vec result{};
+        for (size_t i = 0; i < N; ++i) {
+            result[i] = data[i] + v[i];
+        }
+        return result;
+    }
+
+    void operator+=(const Vec& v) {
         *this = *this + v;
     }
 
-    Vec3 operator-(const Vec3& v) const {
+    Vec operator-(const Vec& v) const {
         return *this + (-v);
     }
 
-    void operator-=(const Vec3& v) {
+    void operator-=(const Vec& v) {
         *this = *this - v;
     }
 
-    Vec3 operator-() const {
-        return Vec3{-x, -y, -z};
+    Vec operator-() const {
+        Vec result{};
+        for (size_t i = 0; i < N; ++i) {
+            result[i] = -data[i];
+        }
+        return result;
     }
 
-    Vec3 operator*(float scale) const {
-        return Vec3{x * scale, y * scale, z * scale};
+    Vec operator*(float scale) const {
+        Vec result{};
+        for (size_t i = 0; i < N; ++i) {
+            result[i] = data[i] * scale;
+        }
+        return result;
     }
 
-    friend Vec3 operator*(float scale, const Vec3& v) {
+    friend Vec operator*(float scale, const Vec& v) {
         return v * scale;
     }
 
     void operator*=(float scale) {
         *this = *this * scale;
     }
+
+  private:
+    std::array<float, N> data;
 };
 
+using Vec2 = Vec<2>;
+using Vec3 = Vec<3>;
